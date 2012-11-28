cd3a1f305e12decf3e4510b78948330ef10cd009
ian <techieyann@gmail.com>
2012-11-27 17:49:59 -0800

    new-business form updated

--- new-business.php
+++ new-business.php
@@ -17,6 +17,19 @@ verify_logged_in();
 //body
 $id = $GLOBALS['b_id'];
 
+$query = "SELECT name FROM business WHERE id='$id'";
+$result = query_db($query);
+$res = mysql_fetch_array($result);
+$name = $res['name'];
+
+$query = "SELECT id, tag FROM tags where id<0 ORDER BY tag ASC";
+$result = query_db($query);
+while($category = mysql_fetch_array($result))
+  {
+	$index = $category['id'];
+	$categories[$index] = $category['tag'];
+  }
+
 //head
 $GLOBALS['header_html_title'] = "tndrbox - ";
 $GLOBALS['header_scripts'] = "";
@@ -34,76 +47,98 @@ disconnect_from_db();
 
 function print_body()
 {
-  global $id;
-  $query = "SELECT name FROM business WHERE id='$id'";
-  $result = query_db($query);
-  $res = mysql_fetch_array($result);
-  $name = $res['name'];
+  global $id, $name, $categories;
+
 
   echo "
-	<div id=\"\" class =\"main-content-pane\">
-		<p>Please enter your business' information.</p>
-		<form name='' enctype='multipart/form-data' action='scripts/new_business.php?id=$id' method='post'>
-			<table>
-				<tr>
-					<td>Business Name</td>
-					<td><input required type=\"text\" name=\"name\" id=\"name\" value=\"$name\" maxlength=\"100\"></td>
-				</tr>
+	<div id='new-business' class=''>
+		<p>Please enter your business information.</p>
+		<form name='new-business-form' enctype='multipart/form-data' action='scripts/new_business.php?id=$id' method='post' class='form-horizontal'>
 
-	   			<tr>
-					<td>Tag 1</td>
-					<td><input required type=\"text\" name=\"tag_1\" id=\"tag_1\" maxlength=\"100\"></td>
-				</tr>
-				<tr>
-					<td>Tag 2</td>
-					<td><input required type=\"text\" name=\"tag_2\" id=\"tag_2\" maxlength=\"100\"></td>
-				</tr>
-				<tr>
-					<td>Address</td>
-					<td><input required type=\"text\" name=\"address\" id=\"address\" maxlength=\"100\"></td>
-				</tr>
-				<tr>
-					<td>City</td>
-					<td><input required type=\"text\" name=\"city\" id=\"city\" maxlength=\"32\" value=\"Oakland\"></td>
-				</tr>
-				<tr>
-					<td>State</td>
-					<td><input required type=\"text\" name=\"state\" id=\"state\" maxlength=\"2\" value=\"Ca\"></td>
-				</tr>
-				<tr>
-					<td>Zip</td>
-					<td><input required type=\"text\" name=\"zip\" id=\"zip\" maxlength=\"5\"></td>
-				</tr>
-				<tr>
-				<td>Number</td>
-				<td><input type=\"text\" name=\"number\" id=\"number\"  maxlength=\"12\"></td>
-			</tr>
-		      	<tr>
-				<td>URL</td>
-				<td><input type=\"text\" name=\"url\" id=\"url\"  maxlength=\"50\"></td>
-			</tr>
-			<tr>
-				<td>Hours</td>
-				<td><input type=\"text\" name=\"hours\" id=\"hours\"  maxlength=\"100\"></td>
-			</tr>
-			<tr>
-				<td>Logo</td>
-				<td>:</td>
-				<td>
-				<input type=\"file\" name=\"logo_upload\" id=\"logo_upload\" size=\"10\">
-				</td>
-			</tr>
-			<tr>
-				<td colspan=\"3\">
-				Note: filesize must be <60Kb
-				</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-		      		<td style=\"border-bottom: solid 1px black;\" align=\"right\"><input type=\"submit\" name=\"submit\" value=\"Upload\"></td>
-			</tr>
-			</table>
+			<div class='control-group'>
+				<label class='control-label' for='name'>
+					Name *
+				</label>
+				<div class='controls'>
+					<input required type='text' maxlength=100 name='name' id='name' value='$name' class='input-xlarge'>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<label class='control-label' for='category'>
+					Category *
+				</label>
+				<div class='controls'>
+					<select required name='category' id='category' autofocus='true'>
+						<option selected='selected'></option>";
+  foreach($categories as $index => $cat)
+	{
+	  echo "
+						<option value='$index'>$cat</option>";
+	}
+	echo "
+					</select>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<label class='control-label' for='address'>
+					Address
+				</label>
+				<div class='controls'>
+					<input type='text' maxlength=100 name='address' id='address' placeholder='Address of your business...' class='input-xlarge'>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<div class='controls'>
+					<input type='text' maxlength=32 name='city' id='city' placeholder='City..' class='input-small'>
+					<input type='text' maxlength=2 name='state' id='state' value='Ca' class='input-mini'>
+					<input type='text' maxlength=5 name='zip' id='zip' placeholder='Zip...' class='input-mini'>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<label class='control-label' for='number'>
+					Number
+				</label>
+				<div class='controls'>
+					<input type='text' maxlength=12 name='number' id='number' placeholder='Phone number...' class='input-medium'>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<label class='control-label' for='url'>
+					URL
+				</label>
+				<div class='controls'>
+					<input type='text' maxlength=50 name='url' id='url' placeholder='Do not include \"http://\"' class='input-xlarge'>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<label class='control-label' for='hours'>
+					Hours
+				</label>
+				<div class='controls'>
+					<input type='text' maxlength=100 name='hours' id='hours' placeholder='Delineate with a comma...' class='input-xlarge'>
+				</div>
+			</div>
+
+			<div class='control-group'>
+				<label class='control-label' for='logo'>
+					Logo
+				</label>
+				<div class='controls'>
+					<input type='file' name='logo' id='logo' class='input-small'>
+					<span class='help-block'>
+						Note: filesize must be <60Kb
+					</span>
+				</div>
+			</div>
+
+			<button type='submit' class='btn btn-primary' id='submit' name='submit'>Submit</button>	
+
 		</form>
 	</div>";
 }
